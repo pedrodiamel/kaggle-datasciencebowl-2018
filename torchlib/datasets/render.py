c2a024e50b15eb65f6acc7660f033d587b5bc9a6
pedrodiamel <pedrodiamel@gmail.com>
2018-05-07 09:21:20 -0300

    update

--- torchlib/datasets/render.py
+++ torchlib/datasets/render.py
@@ -438,7 +438,6 @@ class CircleRender(Render):
 
         images = Render().to_blur(images, sigma=sigma)
         images = Render().to_rgb(images) 
-        #images = cv2.cvtColor(images, cv2.COLOR_GRAY2RGB)
 
         metadata = {
             'centers': centers,
@@ -446,4 +445,77 @@ class CircleRender(Render):
         } 
 
         return images, labels, metadata
+
+
+class EllipseRender(Render):
+    
+    def __init__(self, *args, **kwargs):
+        pass
+
+    @staticmethod
+    def generateellipse( n, m, cnt, rmin, rmax, border, sigma ):
+        
+        mask = np.zeros( (n,m), dtype=np.uint8 );
+        cx = random.randint(border, m-border);
+        cy = random.randint(border, n-border);       
+        aMenor  = random.randint(rmin, rmax);
+        aMayor  = random.randint(rmin, rmax);
+        angle = random.randint(0, 360);    
+
+        h  = random.randint(1, 255);
+        center = [cx, cy]
+        axis = [aMenor,aMayor]
+        mask = cv2.ellipse(mask, (cx,cy), (aMayor, aMenor), angle, 0, 360, 1, -1 ) ;
+        
+        return mask, center, axis, h
+
+
+    @staticmethod
+    def generate(n, m, cnt, rmin, rmax, border, sigma, btouch ):
+        '''
+            @param n,m dim
+            @param cnt
+            @param rmin,rmax
+            @param border
+            @param sigma
+            @param btouch
+
+            # Example       
+            n = 512; m = 512; cnt = 5;
+            rmin = 5; rmax = 50;
+            border = 90;
+            sigma = 20;
+            img, label, meta = CircleRender.generate( n, m, cnt, rmin, rmax, border, sigma, true)        
+        
+        '''
+
+        images = np.ones( (n,m), dtype=np.uint8 );
+        labels = np.zeros( (cnt,n,m), dtype=bool );
+        centers = np.zeros( (cnt,2) );
+        axiss  = np.zeros( (cnt,2) );
+
+        k=0
+        for i in range(cnt):
+            
+            mask, center, axis, h = EllipseRender().generateellipse( n, m, cnt, rmin, rmax, border, sigma )
+            if btouch and Render().istouch(centers[:k,:],axiss[:k,1],center,axis[1]): 
+                continue; 
+
+            images[mask==1] = h
+            labels[k,...] = mask                 
+            centers[i,:] = center;
+            axiss[i,:] = axis;
+            k+=1
+
+        images = Render().to_blur(images, sigma=sigma)
+        images = Render().to_rgb(images) 
+
+        metadata = {
+            'centers': centers,
+            'axis': axiss,
+        } 
+
+        return images, labels, metadata
+
+
 
